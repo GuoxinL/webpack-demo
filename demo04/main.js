8c067e3516707c950e5ce8dc95b895b8ce0dfaf5
Guoxin.Liu <lgx31@sina.cn>
2017-07-12 09:49:31 +0800

    use webpack genernate data-url success

--- demo04/main.js
+++ demo04/main.js
@@ -2,13 +2,13 @@
  * Created by guoxin on 17-7-11.
  */
 var fount = document.createElement("img");
-fount.src = "./drbl-big.jpg";
+fount.src = require("./drbl-big.jpg");
 document.body.appendChild(fount);
 
 var flower = document.createElement("img");
-flower.src = "./drbl-small.jpg";
+flower.src = require("./drbl-small.jpg");
 document.body.appendChild(flower);
 
-var flower = document.createElement("img");
-flower.src = "./emj.png";
-document.body.appendChild(flower);
+var emj = document.createElement("img");
+emj.src = require("./emj.png");
+document.body.appendChild(emj);
