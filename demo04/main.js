d08bccca105fad9487c1d336db3522e5c110b2ae
Guoxin.Liu <lgx31@sina.cn>
2017-07-12 09:10:38 +0800

    url-data bad

---
+++ demo04/main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by guoxin on 17-7-11.
+ */
+var fount = document.createElement("img");
+fount.src = "./drbl-big.jpg";
+document.body.appendChild(fount);
+
+var flower = document.createElement("img");
+flower.src = "./drbl-small.jpg";
+document.body.appendChild(flower);
+
+var flower = document.createElement("img");
+flower.src = "./emj.png";
+document.body.appendChild(flower);
---
+++ demo04/webpack.config.js
@@ -0,0 +1,15 @@
+/**
+ * Created by guoxin on 17-7-11.
+ */
+module.exports = {
+    entry: './main.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.(png|jpg)$/,
+            loader: 'url-loader?limit=8192'
+        }]
+    }
+};
